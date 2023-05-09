7eb33e42d4341ed19e4de1c9ac1829a3e77b30d5
andriy255 <112729376+andriy255@users.noreply.github.com>
2023-05-09 20:26:25 +0300

    Add files via upload

---
+++ 18.py
@@ -0,0 +1,25 @@
+from tkinter import*
+tk=Tk()
+tk.geometry("600x600")
+canvas=Canvas(tk,width=400,height=350)
+canvas.pack()
+flower_image=PhotoImage(file="images.png")
+def btn1_click():
+    lbl2=Label(text=ent.get()+" з днем народження")
+    lbl2.place(x=150,y=150)
+    canvas.create_image(200,150,image=flower_image)
+def btn2_click():
+    print("прощавай ", ent.get(),"!",sep="")
+btn1=Button(text="привітання",command=btn1_click)
+btn1.place(x=75,y=75,width=100,height=50)
+btn2=Button(text="вихід",command=tk.destroy)
+btn2.place(x=225,y=75,width=100,height=50)
+ent=Entry(bd=1)
+ent.place(x=225,y=25,width=100,height=30)
+lbl1=Label(text="ваше імя:") 
+lbl1.place(x=75,y=25,width=100,height=30)
+
+lbl1.pack()
+btn1.pack()
+ent.pack()
+btn2.pack()
